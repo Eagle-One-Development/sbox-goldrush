432dbfbf0974fd94b172904d0411ac50291639de
DoctorGurke <docgurk@gmail.com>
2023-03-11 23:37:35 +0100

    kill player out of bounds

---
+++ code/Entities/GameBoundsEntity.cs
@@ -0,0 +1,25 @@
+﻿using Editor;
+
+namespace GoldRush.Entities;
+
+[HammerEntity, Solid]
+public class GameBoundsEntity : BaseTrigger
+{
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		Tags.Add( "gamebounds" );
+	}
+
+	public override void OnTouchEnd( Entity toucher )
+	{
+		base.OnTouchEnd( toucher );
+
+		if ( toucher is not Player player )
+			return;
+
+		player.TakeDamage( DamageInfo.Generic( 1000f ) );
+		player.RunGameEvent( "player.outofbounds" );
+	}
+}
