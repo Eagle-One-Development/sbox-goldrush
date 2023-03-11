dd19055f6487a0e4c4e5e851ca7fd1cf893bd832
Alex <alex@gu3.me>
2023-03-11 18:57:43 +0000

    Increase player FOV while sprinting

--- code/Systems/Player/PlayerCamera.cs
+++ code/Systems/Player/PlayerCamera.cs
@@ -1,12 +1,18 @@
+using GoldRush.Mechanics;
+
 namespace GoldRush;
 
 public partial class PlayerCamera : EntityComponent<Player>, ISingletonComponent
 {
+	private float _interpolatedFovMultiplier = 1.0f;
+
 	public virtual void Update( Player player )
 	{
+		float fieldOfView = EvaluateFieldOfView( player );
+
 		Camera.Position = player.EyePosition;
 		Camera.Rotation = player.EyeRotation;
-		Camera.FieldOfView = Screen.CreateVerticalFieldOfView( Game.Preferences.FieldOfView );
+		Camera.FieldOfView = Screen.CreateVerticalFieldOfView( fieldOfView );
 		Camera.FirstPersonViewer = player;
 		Camera.ZNear = 0.5f;
 
@@ -17,5 +23,23 @@ public partial class PlayerCamera : EntityComponent<Player>, ISingletonComponent
 		pp.Saturation = 1f;
 		pp.FilmGrain.Response = 1f;
 		pp.FilmGrain.Intensity = 0.01f;
+	}
+
+	/// <summary>
+	/// Calculate a field of view based on the player's current state.
+	/// This will multiply against the field of view specified in the user's preferences.
+	/// </summary>
+	private float EvaluateFieldOfView( Player player )
+	{
+		float fovMultiplier = 1.0f;
+
+		if ( player.Controller.IsMechanicActive<SprintMechanic>() )
+			fovMultiplier = 1.2f;
+
+		// Interpolate so that FOV transitions smoothly
+		_interpolatedFovMultiplier = _interpolatedFovMultiplier.LerpTo( fovMultiplier, 15f * Time.Delta );
+
+		float fieldOfView = Game.Preferences.FieldOfView * _interpolatedFovMultiplier;
+		return fieldOfView;
 	}
 }
