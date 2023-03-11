f4d6c4fa99a53da5e9a4dc75b81bfa47397dbd1c
Alex <alex@gu3.me>
2023-03-11 18:54:04 +0000

    Convert player FOV to vertical FOV, remove vignette

--- code/Systems/Player/PlayerCamera.cs
+++ code/Systems/Player/PlayerCamera.cs
@@ -1,5 +1,3 @@
-using Sandbox;
-
 namespace GoldRush;
 
 public partial class PlayerCamera : EntityComponent<Player>, ISingletonComponent
@@ -8,17 +6,13 @@ public partial class PlayerCamera : EntityComponent<Player>, ISingletonComponent
 	{
 		Camera.Position = player.EyePosition;
 		Camera.Rotation = player.EyeRotation;
-		Camera.FieldOfView = Game.Preferences.FieldOfView;
+		Camera.FieldOfView = Screen.CreateVerticalFieldOfView( Game.Preferences.FieldOfView );
 		Camera.FirstPersonViewer = player;
 		Camera.ZNear = 0.5f;
 
 		// Post Processing
 		var pp = Camera.Main.FindOrCreateHook<Sandbox.Effects.ScreenEffects>();
 		pp.Sharpen = 0.05f;
-		pp.Vignette.Intensity = 0.60f;
-		pp.Vignette.Roundness = 1f;
-		pp.Vignette.Smoothness = 0.3f;
-		pp.Vignette.Color = Color.Black.WithAlpha( 1f );
 		pp.MotionBlur.Scale = 0f;
 		pp.Saturation = 1f;
 		pp.FilmGrain.Response = 1f;
