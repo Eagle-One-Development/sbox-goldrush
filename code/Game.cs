cccee77a797fc6f8bf27622a476abdd715629eb2
Alex <alex@gu3.me>
2023-03-11 18:53:54 +0000

    Add initial code based on https://github.com/DevulTj/sbox-fps_sample

--- code/Game.cs
+++ code/Game.cs
@@ -1,13 +1,25 @@
-﻿using Sandbox;
-using System;
-using System.Linq;
+﻿//
+//
+global using GoldRush.UI;
+global using Sandbox;
+global using Sandbox.UI;
+global using System;
+global using System.Collections.Generic;
+global using System.ComponentModel;
+global using System.Linq;
 
 namespace GoldRush;
 
-public partial class MyGame : GameManager
+public partial class GoldRushGameManager : GameManager
 {
-	public MyGame()
+	public GoldRushGameManager()
 	{
+		if ( Game.IsClient )
+		{
+			Game.RootPanel = new Hud();
+		}
+
+		Game.TickRate = 40;
 	}
 
 	public override void ClientJoined( IClient client )
@@ -15,8 +27,9 @@ public partial class MyGame : GameManager
 		base.ClientJoined( client );
 
 		// Create a pawn for this client to play with
-		var pawn = new Pawn();
+		var pawn = new Player();
 		client.Pawn = pawn;
+		pawn.Respawn();
 
 		// Get all of the spawnpoints
 		var spawnpoints = Entity.All.OfType<SpawnPoint>();
@@ -28,8 +41,16 @@ public partial class MyGame : GameManager
 		if ( randomSpawnPoint != null )
 		{
 			var tx = randomSpawnPoint.Transform;
-			tx.Position = tx.Position + Vector3.Up * 50.0f; // raise it up
+			tx.Position = tx.Position + Vector3.Up * 10.0f; // raise it up
 			pawn.Transform = tx;
 		}
+
+		Chat.AddChatEntry( To.Everyone, client.Name, "joined the game", client.SteamId, true );
+	}
+
+	public override void ClientDisconnect( IClient client, NetworkDisconnectionReason reason )
+	{
+		base.ClientDisconnect( client, reason );
+		Chat.AddChatEntry( To.Everyone, client.Name, "left the game", client.SteamId, true );
 	}
 }
