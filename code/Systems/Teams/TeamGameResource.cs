1e983b464b862109198ccdd7d473230948d4e37e
Alex <alex@gu3.me>
2023-03-11 19:10:27 +0000

    Implement teams as game resources because it's more maintainable

---
+++ code/Systems/Teams/TeamGameResource.cs
@@ -0,0 +1,31 @@
+﻿namespace GoldRush.Teams;
+
+[GameResource( "Gold Rush Team", "grteam", "Defines a team for Gold Rush" )]
+public class TeamGameResource : GameResource, ITeam
+{
+	[Category( "Metadata" )] public string Id { get; set; }
+	[Category( "Metadata" )] public string DisplayName { get; set; }
+	[Category( "Metadata" )] public Color Color { get; set; }
+	[Category( "Metadata" )] public string ScssClass { get; set; }
+
+	public bool Equals( ITeam other )
+	{
+		if ( other is not ITeam )
+			return false;
+
+		return (other.Id == Id);
+	}
+
+	public static bool operator ==( TeamGameResource a, TeamGameResource b )
+	{
+		if ( a is null || b is null )
+			return false;
+
+		return a.Equals( b );
+	}
+
+	public static bool operator !=( TeamGameResource a, TeamGameResource b )
+	{
+		return !(a == b);
+	}
+}
