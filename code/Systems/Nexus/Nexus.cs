5491c215c52947bf2b7695c781ed1dcd3acc2063
Geeknerd1337 <geeknerd1337@gmail.com>
2023-03-12 19:23:16 -0500

    Add basic Nexus class

---
+++ code/Systems/Nexus/Nexus.cs
@@ -0,0 +1,30 @@
+using Sandbox;
+using Sandbox.Utility;
+
+using System.Collections.Generic;
+using Editor;
+
+namespace GoldRush.Nexus;
+
+[Library( "goldrush_nexus" )]
+[HammerEntity]
+[Title( "Nexus" ), Category( "Gameplay" )]
+public partial class Nexus : AnimatedEntity
+{
+	[Property( Title = "The team ID this nexus corresponds to." )]
+	public int TeamId { get; set; }
+
+	[Property( "The max health of the nexus" )]
+	public int MaxHealth { get; set; }
+
+	[Net]
+	public int Health { get; set; }
+
+
+	public override void Spawn()
+	{
+		base.Spawn();
+		Transmit = TransmitType.Always;
+
+	}
+}
