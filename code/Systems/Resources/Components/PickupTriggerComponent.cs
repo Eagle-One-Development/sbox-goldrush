ce14bba6df00ecdd933da59caa79b52ee45f75c1
Alex <alex@gu3.me>
2023-03-14 17:32:17 +0000

    Implement PickupTrigger component, fires event when touched

---
+++ code/Systems/Resources/Components/PickupTriggerComponent.cs
@@ -0,0 +1,25 @@
+﻿namespace GoldRush;
+
+[Prefab, Title( "Pickup Trigger" )]
+public partial class PickupTriggerComponent : EntityComponent
+{
+	[Prefab, Net] public float Radius { get; set; } = 16f;
+	[Prefab, Net] public string EventName { get; set; }
+
+	private PickupTrigger _pickupTrigger;
+
+	protected override void OnActivate()
+	{
+		if ( !Game.IsServer )
+			return;
+
+		_pickupTrigger = new PickupTrigger();
+		_pickupTrigger.SetParent( Entity );
+		_pickupTrigger.SetTriggerSize( Radius );
+		_pickupTrigger.OnTouch += ( e ) =>
+		{
+			if ( e is Player player )
+				player.RunGameEvent( EventName );
+		};
+	}
+}
