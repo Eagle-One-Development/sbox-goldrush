f90ea39efac55375009fffb1f2d5579e6f80f8e5
Alex <alex@gu3.me>
2023-03-14 17:32:43 +0000

    Add more customization to bobbing component (speed, height)

--- code/Systems/Resources/Components/BobbingComponent.cs
+++ code/Systems/Resources/Components/BobbingComponent.cs
@@ -3,8 +3,16 @@
 [Prefab, Title( "Bobbing Component" )]
 public partial class BobbingComponent : EntityComponent
 {
-	[Prefab, Net] public float Speed { get; set; } = 90f;
+	[Prefab, Net] public float RotationSpeed { get; set; } = 90f;
+	[Prefab, Net] public float HeightSpeed { get; set; } = 90f;
 	[Prefab, Net] public float Height { get; set; } = 2f;
+
+	[Net] public float Offset { get; set; } = 0f;
+
+	protected override void OnActivate()
+	{
+		Offset = Game.Random.Float( 0f, 360f );
+	}
 
 	[Event.PreRender]
 	public virtual void OnPreRender()
@@ -15,11 +23,13 @@ public partial class BobbingComponent : EntityComponent
 		if ( !sceneObject.IsValid() )
 			return;
 
+		float time = (Time.Now + Offset);
+
 		// Rotate over time 🔁
-		sceneObject.Rotation = Rotation.From( 0, Time.Now * Speed, 0 );
+		sceneObject.Rotation = Rotation.From( 0, time * RotationSpeed, 0 );
 
 		// Bob up and down ↕️
-		Vector3 bobbingOffset = Vector3.Up * MathF.Sin( Time.Now ) * Height;
+		Vector3 bobbingOffset = Vector3.Up * MathF.Sin( time * HeightSpeed ) * Height;
 		sceneObject.Position = Entity.Position + bobbingOffset;
 	}
 }
