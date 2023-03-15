43c5461c5fe8e3c49a2d30a6ca4c387ad240e2cf
Alex <alex@gu3.me>
2023-03-15 20:34:58 +0000

    Add Purchasable component so we can start doing shop stuff

---
+++ code/Systems/Resources/PurchasableComponent.cs
@@ -0,0 +1,16 @@
+﻿namespace GoldRush;
+
+public enum PurchasableCategory
+{
+	Weapon,
+	Upgrade,
+	Consumable
+}
+
+[Prefab]
+public partial class Purchasable : EntityComponent, ISingletonComponent
+{
+	[Net, Prefab] public int Cost { get; set; } = 69;
+	[Net, Prefab] public PurchasableCategory Category { get; set; } = PurchasableCategory.Weapon;
+	[Net, Prefab] public int Quantity { get; set; } = 1;
+}
