06dd08a0004ad65bff9fbbd4dc3ae4d81f6daebd
Alex <alex@gu3.me>
2023-03-18 03:29:51 +0000

    Add GlowingComponent, uses pickup particle

---
+++ code/Systems/Resources/Components/GlowingComponent.cs
@@ -0,0 +1,13 @@
+﻿namespace GoldRush;
+
+[Prefab, Title( "Glowing Component" )]
+public class GlowingComponent : EntityComponent
+{
+	private Particles Effect;
+
+	protected override void OnActivate()
+	{
+		Effect = Particles.Create( "particles/pickup.vpcf" );
+		Effect.SetEntity( 0, Entity );
+	}
+}
