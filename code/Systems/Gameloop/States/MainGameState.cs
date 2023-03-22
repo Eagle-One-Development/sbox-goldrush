7441d0bff1bd21be8b5050877a11263eeb11b968
DoctorGurke <docgurk@gmail.com>
2023-03-22 14:47:29 +0100

    remove debug logs

--- code/Systems/Gameloop/States/MainGameState.cs
+++ code/Systems/Gameloop/States/MainGameState.cs
@@ -39,23 +39,16 @@ public class MainGameState : GameState
 	[Event( "nexus.destroy" )]
 	private void TryWin()
 	{
-		Log.Info( $"check for win" );
-
 		var teamHash = new HashSet<string>();
-
-		// victim that cannot respawn just died. could be win condition
 
 		foreach ( var client in Game.Clients )
 		{
 			if ( client.Pawn is not Player player ) continue;
 
-			Log.Info( $"check player {player} RSPWN:{player.CanRespawn()} ALIVE:{player.IsAlive}" );
-
 			if ( player.CanRespawn() || player.IsAlive )
 				teamHash.Add( player.Team.Resource.Id );
 		}
 
-		Log.Info( $"team count {teamHash.Count}" );
 		if ( teamHash.Count > 1 ) return;
 
 		GameLoop.FinishWithWin( teamHash.First() );
